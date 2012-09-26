93e03e159dd46798709e27f90c054b0835f48793
nknt <tn478@mail.umkc.edu>
2012-09-26 17:09:38 -0500

    Update DSTBass/src/database/DBAccount.java

--- DSTBass/src/database/DBAccount.java
+++ DSTBass/src/database/DBAccount.java
@@ -9,7 +9,7 @@ import net.roseindia.form.Account;
 public class DBAccount extends DBActionSupport{
 	public Account login(String username, String password) {
 		initializeConnection();
-		// Just test here again
+		// Just test here again and again
 		PreparedStatement pstmt = null;
 		Account account = null;
 		String cmd = "SELECT firstName, lastName, type FROM account WHERE username = ? AND password = ?";
